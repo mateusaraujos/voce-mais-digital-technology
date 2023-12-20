d29eb794d1ac75d18952b58666b34e462f6b29f1
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2023-12-20 03:00:21 -0300

    Conceito de Header definido

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import imgLogo from "../../assets/img/logo.png";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="verde-claro"></div>
+      <div className="verde-escuro"></div>
+      <div className="center-header">
+        <nav>
+          <a href="/">
+            <img className="imgLogo" src={imgLogo} alt="Você mais digital" />
+          </a>
+          <ul>
+            <li>
+              <a href="/cursos">Cursos</a>
+            </li>
+            <li>
+              <a href="/quem-somos">Quem somos</a>
+            </li>
+            <li>
+              <a href="/quero-ser">Quero ser +digital</a>
+            </li>
+          </ul>
+          <a className="portal" href="#">
+            Portal do Aluno
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
