45f1537557fbf2b6b38efd3a2a0190c1a31616cb
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2023-12-21 05:07:57 -0300

    Ambiente preparado para Carrossel de Banners

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,7 @@
+import imgBanner1 from "../assets/img/banners/banner-1.png";
+import imgBanner2 from "../assets/img/banners/banner-2.png";
+import imgBanner3 from "../assets/img/banners/banner-3.png";
+
 export const HEADER_LINKS = [
   {
     url: "#section-one",
@@ -25,5 +29,26 @@ export const TIPOS_CURSOS = [
   {
     url: "#",
     texto: "Evolução Profissional",
+  },
+];
+
+export const BANNERS = [
+  {
+    id: "b-1",
+    imagem: imgBanner1,
+    titulo: "Evolução",
+    descricao: "A evolução do aprendizado digital ao seu alcance.",
+  },
+  {
+    id: "b-2",
+    imagem: imgBanner2,
+    titulo: "Aprenda",
+    descricao: "Aprenda, explore, evolua.",
+  },
+  {
+    id: "b-3",
+    imagem: imgBanner3,
+    titulo: "Prepare-se",
+    descricao: "Prepare-se para o amanhã com nossos cursos.",
   },
 ];
