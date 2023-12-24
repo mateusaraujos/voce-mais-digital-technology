f8b065b427d9aa8debed13c965b1858491149c08
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2023-12-24 13:30:35 -0300

    NotFound

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import NotFound from "../pages/NotFound/NotFound";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
