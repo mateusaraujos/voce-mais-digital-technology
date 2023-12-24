7e078c03a4ef42702da9106a14c871816964e9e2
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2023-12-24 04:16:54 -0300

    Algumas mudanças no design de links e paddings

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 export default function Links({ url, texto }) {
   return (
     <li>
-      <a href={url}>{texto}</a>
+      <a className="links-header" href={url}>
+        {texto}
+      </a>
     </li>
   );
 }
