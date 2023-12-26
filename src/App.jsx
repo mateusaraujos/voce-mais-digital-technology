67d0affb73d93b6f22ae080ee8064f0665cd37b0
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2023-12-26 17:59:38 -0300

    Tela inicial com animação

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import initialLogo from "./assets/img/logo-inicial.png";
 
 export default function App() {
+  const [carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCarregando(false);
+    }, 1000);
+  }, []);
+
+  if (carregando) {
+    return (
+      <div className="position-fixed">
+        <img
+          className="img-fluid"
+          src={initialLogo}
+          alt="Você + Digital Technology"
+          draggable="false"
+        />
+      </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <AppRouter />
