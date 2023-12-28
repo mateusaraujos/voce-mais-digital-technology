b1cc2de29caba6fcb3b2cd4acfa8e2b7ed445a30
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2023-12-28 11:31:28 -0300

    Conceito de VenhaSerMais definido

---
+++ src/components/VenhaSerMais/VenhaSerMais.jsx
@@ -0,0 +1,29 @@
+import "./VenhaSerMais.css";
+
+export default function VenhaSerMais() {
+  return (
+    <section id="section-four">
+      <div className="center-section">
+        <h2>
+          <span className="thin">Venha ser</span> +digital
+        </h2>
+        <div className="section-venhaser">
+          <div>
+            <h3>
+              Preencha os campos do formulário
+              <br /> para que nossa equipe entre em
+              <br />
+              contato com você e forneça
+              <br /> mais detalhes em breve.
+            </h3>
+            <h4>
+              Seu futuro digital
+              <br /> começa aqui!
+            </h4>
+          </div>
+          <div>FORMULÁRIO</div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import EscolhaCurso from "../../components/EscolhaCurso/EscolhaCurso";
 import SomosMais from "../../components/SomosMais/SomosMais";
 import MapaLocal from "../../components/MapaLocal/MapaLocal";
 import Footer from "../../components/Footer/Footer";
+import VenhaSerMais from "../../components/VenhaSerMais/VenhaSerMais";
 import { BANNERS } from "../../data/data";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <EscolhaCurso />
         <SomosMais />
         <MapaLocal />
+        <VenhaSerMais />
       </main>
       <Footer />
     </div>
