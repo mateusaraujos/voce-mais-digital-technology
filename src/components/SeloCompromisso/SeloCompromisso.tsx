857498ed72e8d4a302c7cb8c23809dfd1cb81df9
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2024-01-03 12:01:14 -0300

    Nomes de estilos atualizados para SeloCompromisso

--- src/components/SeloCompromisso/SeloCompromisso.tsx
+++ src/components/SeloCompromisso/SeloCompromisso.tsx
@@ -6,8 +6,8 @@ export default function SeloCompromisso() {
   return (
     <article>
       <div className="center-article">
-        <div className="article-compromisso">
-          <img className="selo" src={imgSelo} alt="Selo" />
+        <div className="article-commitment">
+          <img className="stamp" src={imgSelo} alt="Selo" />
           <p>
             Compromisso absoluto com a<br /> excelência em cada curso.
           </p>
