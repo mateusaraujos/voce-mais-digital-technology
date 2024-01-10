3146ca0f3668300d9875982be75377201c9ab2f3
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2024-01-10 18:44:38 -0300

    Header no modo dark

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,11 +1,40 @@
+import styled from "styled-components";
+
 import { HeaderLink } from "../types/appTypes";
+import { CustomTheme } from "../types/themeTypes";
+import { useTheme } from "../hooks/useTheme";
+
+const HeaderLinks = styled.a<{ theme: CustomTheme }>`
+  color: ${(props) =>
+    props.theme.mode === "light"
+      ? "var(--primary-dark)"
+      : "var(--primary-white)"};
+
+  border-radius: 4px;
+  padding: 2px 10px;
+
+  border: ${(props) =>
+    props.theme.mode === "light"
+      ? "1px solid var(--dark-green)"
+      : "1px solid var(--light-green)"};
+
+  &::before {
+    border-radius: 4px;
+    background-color: ${(props) =>
+      props.theme.mode === "light"
+        ? "var(--dark-green)"
+        : "var(--light-green)"};
+  }
+`;
 
 export default function Links({ url, text }: HeaderLink) {
+  const { theme } = useTheme();
+
   return (
     <li>
-      <a className="page-links" href={url}>
+      <HeaderLinks theme={theme} className="page-links" href={url}>
         {text}
-      </a>
+      </HeaderLinks>
     </li>
   );
 }
