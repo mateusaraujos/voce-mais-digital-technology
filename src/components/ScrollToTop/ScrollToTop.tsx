eea2a984e4662d572044a9d4c2f444c93e9613d6
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2024-01-12 17:52:36 -0300

    Componente ScrollToTop criado

---
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+
+    if (scrollY > 100) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    };
+
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
+    return (
+      <button
+        className={`scroll-to-top ${isVisible ? "visible" : ""}`}
+        onClick={scrollToTop}
+      >
+        &#8679; Top
+      </button>
+    );
+  };
+}
