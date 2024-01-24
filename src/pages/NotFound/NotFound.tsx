12cb0b4e9ace2e1c7b70ecd0f35463c9b653378c
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2024-01-24 19:25:14 -0300

    Helmet para página NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,23 @@
+import { Helmet } from "react-helmet";
+
 export default function NotFound() {
-  return <h1>Página não encontrada!</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Página não encontrada | Você+Digital</title>
+
+        <meta
+          name="description"
+          content="Página de erro personalizada para a Você+Digital"
+        />
+
+        <meta
+          name="keywords"
+          content="Erro, 404, NotFound, React, TypeScript"
+        />
+      </Helmet>
+
+      <h1>Página não encontrada!</h1>
+    </>
+  );
 }
